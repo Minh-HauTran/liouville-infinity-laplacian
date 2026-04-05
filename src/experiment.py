a0b1aff4a58cd28f18d16fea2763e3915b0fc05b
Minh-Hau Tran <tranminhhau9a2thcsttlp@gmail.com>
2026-04-06 02:20:29 +0700

    Update experiment.py

--- src/experiment.py
+++ src/experiment.py
@@ -1,5 +1,7 @@
-from train import train_pinn
+from src.train import train_pinn
 import matplotlib.pyplot as plt
+import torch
+import os
 
 def phase_study(q_values):
     results = {}
@@ -19,8 +21,19 @@ if __name__ == "__main__":
     q_values = [1,2,3,4,5]
     results = phase_study(q_values)
 
+
+    os.makedirs("experiments/phase_transition", exist_ok=True)
+
+    plt.figure(figsize=(12,5))
+
     for q, (x, u) in results.items():
         plt.plot(x, u, label=f"q={q}")
 
     plt.legend()
+    plt.title("Phase Transition (q)")
+
+ 
+    plt.savefig("experiments/phase_transition/q_plot.png", dpi=300)
+
+
     plt.show()
